58a56d30af1e126eca2c05924f34170c89b9546a
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-07-19 06:15:00 +0000

    added edit to redirect to new edit form.  need to append edit form

--- app/app.js
+++ app/app.js
@@ -17,28 +17,40 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
-app.use(methodOverride(function(req, res){
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    var method = req.body._method
-    delete req.body._method
-    return method
-  }
+app.use(methodOverride(function(req, res) {
+	if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+		// look in urlencoded POST bodies and delete it
+		var method = req.body._method
+		delete req.body._method
+		return method
+	}
 }))
 
-app.get('/', galleryList)
-app.get('/gallery/:id', renderPictureById);
+app.get('/', renderGallery)
+
+app.route('/gallery/:id')
+	.get(renderPictureById)
+	.put(editPhoto)
+	.delete(deletePhoto);
 
 app.route('/gallery')
-	.get(galleryList)
+	.get(renderGallery)
 	.post(addNewPhoto)
-	.put(editPhoto)
+	.put(editPhoto);
 
-app.delete('/gallery/:id', function(req, res) {
-	console.log('we are in here');
+function deletePhoto(req, res) {
+	var id = req.params.id;
+	Photo.find({
+		where: {
+			id: id
+		}
+	}).then(function(photo) {
+		console.log('photo', photo);
+		photo.destroy();
+	});
 	res.send('ok');
-});
 
+}
 
 
 function renderPictureById(req, res) {
@@ -55,7 +67,10 @@ function renderPictureById(req, res) {
 			res.render('photoById', {
 				photo: photo,
 				createDeleteLink: function(id) {
-					return '<form action="/gallery/' + id + '" method="POST"><input type="hidden" name="_method" value="DELETE"><button> delete </button></form>';
+					return '<form action="/gallery/' + id + '" method="POST"><input type="hidden" name="_method" value="DELETE"><button> Delete Photo </button></form>';
+				},
+				createEditLink: function(id) {
+					return '<a href="gallery/'+id+'"/edit><form action="/gallery/' + id + '" method="PUT"><input type="hidden" name="_method" value="PUT"><button> Edit Photo </button></form></a>';
 				}
 			});
 		}
@@ -76,7 +91,7 @@ function validatePost(body) {
 
 //get
 
-function galleryList(req, res) {
+function renderGallery(req, res) {
 	Photo.findAll().then(function(photos) {
 
 		res.render('index', {
@@ -98,10 +113,26 @@ function addNewPhoto(req, res) {
 		res.send('Invalid keys in form');
 	}
 }
-//post
+
+
 function editPhoto(req, res) {
+	var id = req.params.id;
+
 	if (validatePost(req.body)) {
-		res.send('editable');
+		Photo.update({
+			author: req.body.author,
+			link: req.body.link,
+			description: req.body.description
+		}, {
+			where: {
+				id: id
+			}
+		}).then(function(arg1, arg2) {
+			console.log('arg1',arg1);
+			console.log('arg2',arg2);
+			res.write('pau');
+		});
+		// addNewPhoto(req,res);
 	} else {
 		res.send('Invalid keys in form');
 	}
